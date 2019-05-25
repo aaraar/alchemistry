1ee9e901bc04dda8bc5b10380e4b6872d93ef4f4
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2019-05-25 21:32:47 +0200

    Edited eslint to accept prettier formats

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { onCreateProfile, onProfileAll, onProfile } from "./profiles";
 
-import users from "./profiles";
 const router = express.Router();
 
 router
@@ -13,9 +12,9 @@ router
   .post("/users/create", onCreateProfile)
   .get("/users", onProfileAll);
 
-// router.use(function(req, res, next) {
-//   res.status(404).send("Sorry can't find that!");
-// });
+router.use(function(req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+});
 
 function onHome(req, res) {
   res.render("home", { data: "hello world" });
