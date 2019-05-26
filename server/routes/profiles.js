5bd4245f0bb53747a0d3cc63d699d5b9203efb0a
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2019-05-26 21:51:48 +0200

    Init mongodb

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -1,36 +1,17 @@
 import moment from "moment";
 import slug from "slug";
 import profiles from "../../db/profiles.json";
-// const profileObj = {
-//   profiles: [
-//     {
-//       id: "edward-elric",
-//       name: "Edward Elric",
-//       background: "Haarlem",
-//       dndClass: "Male",
-//       bday: "1996-05-24",
-//       faction: "5e",
-//       interest: "female",
-//       traits: "Hot headed, philantropic, short-tempered, friendly",
-//       ideals: "Resurect his mother and find his brothers body back",
-//       bonds: "Does everything for family and friends",
-//       flaws: "Bad at predicting danger"
-//     },
-//     {
-//       id: "je-broer",
-//       name: "Je Broer",
-//       background: "Haarlem",
-//       dndClass: "Male",
-//       bday: "1996-08-30",
-//       faction: "5e",
-//       interest: "female",
-//       traits: "Hot headed, philantropic, short-tempered, friendly",
-//       ideals: "Resurect his mother and find his brothers body back",
-//       bonds: "Does everything for family and friends",
-//       flaws: "Bad at predicting danger"
-//     }
-//   ]
-// };
+import mongo from "mongodb";
+let db = null;
+const url = "mongodb://" + process.env.DB_HOST + ":" + process.env.DB_PORT;
+
+mongo.MongoClient.connect(url, function(err, client) {
+  if (err) {
+    throw err;
+  }
+
+  db = client.db(process.env.DB_NAME);
+});
 
 export function onCreateProfile(req, res) {
   res.render("profileCreation");
@@ -54,44 +35,69 @@ export function onCreateProfilePost(req, res) {
   res.redirect(`/user/${id}`);
 }
 
-export function onProfileAll(req, res) {
-  profileList.forEach(profile => {
-    profile.level = moment().diff(profile.bday, "years");
-    profile.race = "Human";
-    console.log(profile);
-  });
-  res.render("profiles", profiles);
+export function onProfileAll(req, res, next) {
+  db.collection("users")
+    .find()
+    .toArray(done);
+
+  function done(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      let profiles = { data: data };
+      profiles.data.forEach(profile => {
+        profile.level = moment().diff(profile.bday, "years");
+        profile.race = "Human";
+        console.log(profile);
+      });
+      res.render("profiles", profiles);
+    }
+  }
 }
 
-export function onProfile(req, res) {
+export function onProfile(req, res, next) {
   const id = req.params.id;
-  const profile = profiles.find(value => value.id === id);
-  profile.level = moment().diff(profile.bday, "years");
-  switch (profile.faction) {
-    case "5e":
-      profile.faction = "5th Edition";
-      break;
-    case "3.5e":
-      profile.faction = "3.5 Edition";
-      break;
-    case "pathfinder":
-      profile.faction = "Pathfinder";
-      break;
-    case "3e":
-      profile.faction = "3rd Edition";
-      break;
-    case "4e":
-      profile.faction = "4th Edition";
-      break;
+  db.collection("users").findOne(
+    {
+      _id: new mongo.ObjectID(id)
+    },
+    done
+  );
+
+  function done(err, profile) {
+    if (err) {
+      next(err);
+    } else {
+      profile.level = moment().diff(profile.bday, "years");
+      switch (profile.faction) {
+        case "5e":
+          profile.faction = "5th Edition";
+          break;
+        case "3.5e":
+          profile.faction = "3.5 Edition";
+          break;
+        case "pathfinder":
+          profile.faction = "Pathfinder";
+          break;
+        case "3e":
+          profile.faction = "3rd Edition";
+          break;
+        case "4e":
+          profile.faction = "4th Edition";
+          break;
+      }
+      let interest = "";
+      for (let i = 0; i < profile.interest.length; i++) {
+        if (i === 0) interest += profile.interest.charAt(0).toUpperCase();
+        else interest += profile.interest[i];
+      }
+      profile.interest = interest;
+      profile.race = "Human";
+      profile.experience =
+        365 -
+        moment(profile.bday.slice(5)).diff(moment().format("MM-DD"), "days");
+
+      res.render("profile", profile);
+    }
   }
-  let interest = "";
-  for (let i = 0; i < profile.interest.length; i++) {
-    if (i === 0) interest += profile.interest.charAt(0).toUpperCase();
-    else interest += profile.interest[i];
-  }
-  profile.interest = interest;
-  profile.race = "Human";
-  profile.experience =
-    365 - moment(profile.bday.slice(5)).diff(moment().format("MM-DD"), "days");
-  res.render("profile", profile);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
-const environment = (process.env.NODE_ENV || 'development').trim();
+const environment = (process.env.NODE_ENV || "development").trim();
 
-if (environment === 'development') {
-
-module.exports = require('./config/webpack.dev.config.js');
-
+if (environment === "development") {
+  module.exports = require("./config/webpack.dev.config.js");
 } else {
-
-module.exports = require('./config/webpack.prod.config.js');
-
+  module.exports = require("./config/webpack.prod.config.js");
 }
