adbd7eab11b47801b60a48dbc080b96b33d452df
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2019-05-27 16:40:07 +0200

    Added mongodb support for profile creation

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,31 +1,36 @@
 import express from "express";
-import { onCreateProfile, onProfileAll, onProfile } from "./profiles";
+import {
+	onCreateProfile,
+	onProfileAll,
+	onProfile,
+	onCreateProfilePost
+} from "./profiles";
 
 const router = express.Router();
 
 router
-  .get("/", onHome)
-  .get("/about", onAbout)
-  .get("/login", onLogin)
-  .get("/user/:id", onProfile)
-  .get("/users/create", onCreateProfile)
-  .post("/users/create", onCreateProfile)
-  .get("/users", onProfileAll);
+	.get("/", onHome)
+	.get("/about", onAbout)
+	.get("/login", onLogin)
+	.get("/user/:id", onProfile)
+	.get("/users/create", onCreateProfile)
+	.post("/users/add", onCreateProfilePost)
+	.get("/users", onProfileAll);
 
 router.use(function(req, res, next) {
-  res.status(404).send("Sorry can't find that!");
+	res.status(404).send("Sorry can't find that!");
 });
 
 function onHome(req, res) {
-  res.render("home", { data: "hello world" });
+	res.render("home", { data: "hello world" });
 }
 
 function onAbout(req, res) {
-  n;
-  res.send("about");
+	n;
+	res.send("about");
 }
 function onLogin(req, res) {
-  res.send("profile");
+	res.send("profile");
 }
 
 export default router;
--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -2,106 +2,114 @@ import moment from "moment";
 import slug from "slug";
 import profiles from "../../db/profiles.json";
 import mongo from "mongodb";
-let db = null;
+const MongoClient = mongo.MongoClient;
+
+var db = "db";
 const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}`;
 
-mongo.MongoClient.connect(url, function(err, client) {
-  if (err) {
-    throw err;
-  }
+class User {
+	constructor(userObj) {
+		for (var attributeName in userObj) {
+			this[attributeName] = userObj[attributeName];
+		}
+	}
+	merge(object) {
+		for (var attributeName in object) {
+			this[attributeName] = object[attributeName];
+		}
+		return this;
+	}
+}
 
-  db = client.db(process.env.DB_NAME);
+MongoClient.connect(url, function(err, client) {
+	if (err) {
+		throw err;
+	}
+
+	db = client.db(process.env.DB_NAME);
 });
 
 export function onCreateProfile(req, res) {
-  res.render("profileCreation");
+	res.render("profileCreation");
 }
 
 export function onCreateProfilePost(req, res) {
-  const id = slug(req.body.name).toLowerCase();
-  profiles.push({
-    id,
-    name: req.body.name,
-    background: req.body.background,
-    dndClass: req.body.dndClass,
-    bday: req.body.bday,
-    faction: req.body.faction,
-    interest: req.body.interest,
-    traits: req.body.traits,
-    ideals: req.body.ideals,
-    bonds: req.body.bonds,
-    flaws: req.body.flaws
-  });
-  res.redirect(`/user/${id}`);
+	const id = slug(req.body.name).toLowerCase();
+	let newUser = new User(req.body);
+	newUser.id = id;
+	db.collection("users").insertOne(newUser, (err, res) => {
+		if (err) throw err;
+		console.log("1 new user added");
+		res.redirect(`/user/${res.insertedId}`);
+	});
 }
 
 export function onProfileAll(req, res, next) {
-  db.collection("users")
-    .find()
-    .toArray(done);
+	db.collection("users")
+		.find()
+		.toArray(done);
 
-  function done(err, data) {
-    if (err) {
-      next(err);
-    } else {
-      let profiles = { data: data };
-      profiles.data.forEach(profile => {
-        profile.level = moment().diff(profile.bday, "years");
-        profile.race = "Human";
-        console.log(profile);
-      });
-      res.render("profiles", profiles);
-    }
-  }
+	function done(err, data) {
+		if (err) {
+			next(err);
+		} else {
+			let profiles = { data };
+			profiles.data.forEach(profile => {
+				profile.level = moment().diff(profile.bday, "years");
+				profile.race = "Human";
+			});
+			res.render("profiles", profiles);
+		}
+	}
 }
 
 export function onProfile(req, res, next) {
-  const id = req.params.id;
-  db.collection("users").findOne(
-    {
-      _id: new mongo.ObjectID(id)
-    },
-    done
-  );
+	const id = req.params.id;
+	db.collection("users").findOne(
+		{
+			_id: new mongo.ObjectID(id)
+		},
+		done
+	);
 
-  function done(err, profile) {
-    if (err) {
-      next(err);
-    } else {
-      profile.level = moment().diff(profile.bday, "years");
-      switch (profile.faction) {
-        case "5e":
-          profile.faction = "5th Edition";
-          break;
-        case "3.5e":
-          profile.faction = "3.5 Edition";
-          break;
-        case "pathfinder":
-          profile.faction = "Pathfinder";
-          break;
-        case "3e":
-          profile.faction = "3rd Edition";
-          break;
-        case "4e":
-          profile.faction = "4th Edition";
-          break;
-      }
-      let interest = "";
-      for (let i = 0; i < profile.interest.length; i++) {
-        if (i === 0) interest += profile.interest.charAt(0).toUpperCase();
-        else interest += profile.interest[i];
-      }
-      profile.interest = interest;
-      profile.race = "Human";
-      profile.experience =
-        365 -
-        moment(profile.bday.slice(5)).diff(moment().format("MM-DD"), "days");
-      profile.experience =
-        profile.experience >= 365
-          ? profile.experience - 365
-          : profile.experience;
+	function done(err, profile) {
+		if (err) {
+			next(err);
+		} else {
+			profile.level = moment().diff(profile.bday, "years");
+			switch (profile.faction) {
+				case "5e":
+					profile.faction = "5th Edition";
+					break;
+				case "3.5e":
+					profile.faction = "3.5 Edition";
+					break;
+				case "pathfinder":
+					profile.faction = "Pathfinder";
+					break;
+				case "3e":
+					profile.faction = "3rd Edition";
+					break;
+				case "4e":
+					profile.faction = "4th Edition";
+					break;
+			}
+			let interest = "";
+			for (let i = 0; i < profile.interest.length; i++) {
+				if (i === 0) interest += profile.interest.charAt(0).toUpperCase();
+				else interest += profile.interest[i];
+			}
+			profile.interest = interest;
+			profile.race = "Human";
+			profile.experience =
+				365 -
+				moment(profile.bday.slice(5)).diff(moment().format("MM-DD"), "days");
+			profile.experience =
+				profile.experience >= 365
+					? profile.experience - 365
+					: profile.experience;
 
-      res.render("profile", profile);
-    }
-  }
+			res.render("profile", profile);
+		}
+	}
 }
